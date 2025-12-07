77b07d88c882a399740d1359d9f2e096c6a248fa
mtsweet2022 <mtsweet2022@gmail.com>
2025-12-07 15:58:25 -0600

    a

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,5 @@
 
 ScrollReveal().reveal('.headline');
 
-     
-     swal.fire({
-          title: "Welcome to Fun and Fantasy!",
-          text: "Please note that links in this site may be unfinished, and lead to errors when clicked.",
-          icon: "info",
-          confirmButtonText: "Start Exploring",
-        });
+
       
