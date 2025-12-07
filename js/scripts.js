266c10a9242d949ae7daba723af1b3a1fdcf97a8
mtsweet2022 <mtsweet2022@gmail.com>
2025-12-07 13:03:19 -0600

    I'm going to go crazy at this rate

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,5 @@ ScrollReveal().reveal('.headline');
 swal.fire({
           title: "Welcome to Fun and Fantasy!",
           text: "Explore a world of imagination and adventure. Please be aware that links may be unfinished, and will produce errors when clicked.",
-
           confirmButtonText: "Start Exploring",
         });
